01e1fcd6994fb2d7f2e001e48f952b4ff7cfc603
Devilbull <mbeluci512@gmail.com>
2025-12-14 21:35:34 +0100

    It should work ___VA

--- userservice/src/main/java/com/vuckoapp/userservice/model/GamerStats.java
+++ userservice/src/main/java/com/vuckoapp/userservice/model/GamerStats.java
@@ -11,7 +11,7 @@ import java.util.UUID;
 @NoArgsConstructor @AllArgsConstructor @Builder
 public class GamerStats {
     @Id
-    @Column(columnDefinition = "uuid")
+    @Column(name = "user_id", columnDefinition = "uuid")
     private UUID id;
 
     @OneToOne
@@ -19,27 +19,28 @@ public class GamerStats {
     @JoinColumn(name = "user_id")
     private User user;
 
-    @Column(nullable = false)
+    @Column(name = "total_sessions", nullable = false)
     private int totalSessions = 0;
 
-    @Column(nullable = false)
+    @Column(name = "attended_sessions", nullable = false)
     private int numberOfJoinedSessions = 0;
 
-    @Column(nullable = false)
+    @Column(name = "missed_sessions", nullable = false)
     private int numberOfLeavingSessions = 0;
 
     @Column(name = "attendance_rate", nullable = false, precision = 5, scale = 2)
     private BigDecimal attendanceNumber = BigDecimal.valueOf(100.0);
 
-    @Column(nullable = false)
+    @Column(name = "successful_hosted_sessions", nullable = false)
     private int numberOfSuccessfulSessions = 0;
 
     @Enumerated(EnumType.STRING)
-    @Column(nullable = false, length = 20)
-    private OrganizerTitle organizerTitle =  OrganizerTitle.BRONZE;
+    @Column(name = "organizer_title", nullable = false, length = 20)
+    private OrganizerTitle organizerTitle = OrganizerTitle.BRONZE;
 
     @PrePersist
     public void prePersist() {
         if (id == null) id = UUID.randomUUID();
     }
 }
+
