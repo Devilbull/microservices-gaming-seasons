597faa12606d63cd9ddbb818271d3d5b90c536e9
Devilbull <mbeluci512@gmail.com>
2025-12-15 03:14:03 +0100

    Added test of sending mail

--- notificationservice/src/main/java/com/vuckoapp/notificationservice/controller/TestController.java
+++ notificationservice/src/main/java/com/vuckoapp/notificationservice/controller/TestController.java
@@ -1,4 +1,24 @@
 package com.vuckoapp.notificationservice.controller;
 
+import com.vuckoapp.notificationservice.dto.NotificationRequest;
+import com.vuckoapp.notificationservice.service.NotificationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
 public class TestController {
+
+    private final NotificationService notificationService;
+
+    @GetMapping("/test-mail")
+    public void test() {
+        NotificationRequest req = new NotificationRequest(
+                "aca.vdj@gmail.com",
+                "Test",
+                "Radi 👍"
+        );
+        notificationService.sendNotification(req);
+    }
 }
